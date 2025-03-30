34fe1bbd30e4fa4a7cf6824e7fbf28ea112a1eec
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-03-29 21:23:18 -0700

    implemented jwt authorization and controller for login,register, and role controll

---
+++ backend/Data/BackendContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace backend.Data
+{
+    public class BackendContext : IdentityDbContext<IdentityUser>
+    {
+
+        public BackendContext(DbContextOptions options):base(options)
+        { 
+        
+        }
+
+
+    }
+}
