97ec13e09a7b83b5d9a5c900778b7c6cd6d7ef92
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-04-20 00:10:50 -0700

    added token generation to login and applied middleware to chat routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -30,7 +30,14 @@ export const login = async (req, res) => {
         return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        return res.status(200).json({ message: 'Login successful', user: { id: user.Id, email: user.Email } });
+        
+        const token = jwt.sign(
+            { id: user.Id, email: user.Email }, // payload
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' } // token expiry
+        );
+        
+        return res.status(200).json({ message: 'Login successful', token: token });
 
     } catch (err) {
         console.error('Login error:', err);
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { createChat, getChats, addChatMember } from '../controllers/chatController';
+import { verifyToken } from '../middleware/verifyToken';
+
 const router = express.Router();
 
-router.post('/create', createChat);
+router.post('/create', verifyToken, createChat);
 
-router.get('/getChats', getChats);
+router.get('/getChats', verifyToken, getChats);
 
-router.post('/addChatMember', addChatMember);
+router.post('/addChatMember', verifyToken, addChatMember);
 
 export default router;
