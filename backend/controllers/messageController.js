99556d6f50283aabbc51d1a153e34d02fac16878
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-04-22 17:07:53 -0700

    added send message api to backend controller and route

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,27 @@
+import { sql, poolPromise } from '../db.js'; // adjust the path as needed
+
+
+export const sendMessage = async (req, res) => {
+
+    const { conversationId, message } = req.body;
+
+    const SenderId = req.user.id;
+    try{
+
+        const pool = await poolPromise;
+
+        await pool.request()
+            .input('ConversationId', sql.Int, conversationId)
+            .input('SenderId', sql.Int, SenderId)
+            .input('Content', sql.NVarChar, message)
+            .query(`
+                INSERT INTO Messages (ConversationId, SenderId, Content)
+                VALUES (@ConversationId, @SenderId, @Message)
+            `);
+
+    }catch(err){
+            
+            console.error('send message error:', err);
+            return res.status(500).json({ message: 'Server error' });
+    }
+}
