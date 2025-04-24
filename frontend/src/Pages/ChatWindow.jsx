fcb223ca6b57d61832f510a22be38b14047599fd
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-04-24 13:50:28 -0700

    fixed bug where switching between chats woulnt reload messages

--- frontend/src/Pages/ChatWindow.jsx
+++ frontend/src/Pages/ChatWindow.jsx
@@ -37,7 +37,7 @@ const ChatWindow = () => {
     if (token) {
       fetchMessages(token);
     }
-  }, [token]);
+  }, [token,location]);
 
   const fetchMessages = async (token) => {
     setLoading(true);
